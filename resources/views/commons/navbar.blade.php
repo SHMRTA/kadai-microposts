02b1b89848aa6f41e4c997aab327329b1ae0fb9e
SHR <shimryta@gmail.com>
2020-10-14 04:55:25 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,22 @@
+<header>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        {{-- トップページへのリンク --}}
+        <a class="navbar-brand" hre="/">Microposts</a>
+        
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggle-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                {{-- ユーザー登録ページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">Sign up</a></li>
+                
+                {{-- ログインページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+            </ul>
+            
+        </div>
+    </nav>
+</header>
